b5c228196b99cc9f4dc413ddd6f7f0a40aede144
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2024-11-05 09:00:25 +0100

    dodanie operatory

---
+++ Operatory09.js
@@ -0,0 +1,28 @@
+//task1
+console.log("Zadanie 1");
+console.log(2*3+1);// expected 7
+console.log(2**4); // expected 16
+console.log(5*1); // expected 5
+console.log(8**2-5**2);// expected 39
+//task2
+console.log("Zadanie 2");
+console.log(4*5==20);
+console.log(6*5==30);
+console.log(-17<0);
+console.log(25>1);
+console.log(2+2<2*4);
+//task3
+console.log("Zadanie 3");
+console.log(true!=false);
+console.log(false==false);
+console.log(false!=false<true);
+console.log(true>false==false<true);
+//task4
+console.log("Zadanie 4");
+let a = 10;
+let b = 5;
+let c;
+c=a+b;
+console.log("Oto wartosc zmiennej c: ",c);
+console.log(a>=0 && b>=0);
+
