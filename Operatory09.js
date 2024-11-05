7a7a3fc7cddebb772e11265f2cd51235fd30b389
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2024-11-05 09:33:37 +0100

    modyfikacja pliku

--- Operatory09.js
+++ Operatory09.js
@@ -25,4 +25,47 @@ let c;
 c=a+b;
 console.log("Oto wartosc zmiennej c: ",c);
 console.log(a>=0 && b>=0);
+//task5
+console.log("Zadanie 5");
+let x =3;
+let y =4;
+let z =5;
+let najwieksza = Math.max(x,y,z);
+if(najwieksza==y)
+{
+    if(y<x+z)
+    {
+        console.log("Da sie stworzyc trojkat.");
+        let s =(y+x+z)/2;
+        let pole = Math.sqrt(s*(s-x)*(s-y)*(s-z));
+        console.log("Pole trojkata wynosi: ",pole);
+    }
+    else
+    {console.log("Nie sie stworzyc trojkat.");}
+}
+if(najwieksza==x)
+{
+    if(x<y+z)
+    {
+        console.log("Da sie stworzyc trojkat.");
+        let s =(y+x+z)/2;
+        let pole = Math.sqrt(s*(s-x)*(s-y)*(s-z));
+        console.log("Pole trojkata wynosi: ",pole);
+    }
+    else
+    {console.log("Nie sie stworzyc trojkat.");}
+}
+if(najwieksza==z)
+{
+    if(z<x+y)
+    {
+        console.log("Da sie stworzyc trojkat.");
+        let s =(y+x+z)/2;
+        let pole = Math.sqrt(s*(s-x)*(s-y)*(s-z));
+        console.log("Pole trojkata wynosi: ",pole);
+    }
+    else
+    {console.log("Nie sie stworzyc trojkat.");}
+}
+
 
